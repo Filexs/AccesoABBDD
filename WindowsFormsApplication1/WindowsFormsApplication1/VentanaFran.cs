a2d3a466cf72eedc0639d287a9cd67f4a0ec06f0
Filexs <franciscodelvallebas@gmail.com>
2014-05-23 17:36:58 +0200

    Adición tabla clientes

--- WindowsFormsApplication1/WindowsFormsApplication1/VentanaFran.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/VentanaFran.cs
@@ -16,5 +16,20 @@ namespace WindowsFormsApplication1
         {
             InitializeComponent();
         }
+
+        private void clientesBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.clientesBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.northwind_2007DataSet);
+
+        }
+
+        private void VentanaFran_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'northwind_2007DataSet.Clientes' Puede moverla o quitarla según sea necesario.
+            this.clientesTableAdapter.Fill(this.northwind_2007DataSet.Clientes);
+
+        }
     }
 }
