94a5efbe41c3dac2f6fb03b50631a26de505e2ae
Filexs <franciscodelvallebas@gmail.com>
2014-05-23 17:16:35 +0200

    Programa Básico
    El programa básico contiene 3 botones con sus respectivas ventanas y una
    conexión con la bbdd de ejemplo de acces "NorthWind"

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/VentanaInicial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class VentanaInicial : Form
+    {
+        public VentanaInicial()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new VentanaFran().ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            new VentanaAlvaro().ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            new VentanaPedro().ShowDialog();
+        }
+    }
+}
